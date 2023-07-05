1ec903deeefb268231739a703503e9ad3406f38e
Vic <vicjs612@gmail.com>
2023-07-05 16:49:54 -0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import SubHeading from "./SubHeading/SubHeading";
+import FooterOverlay from "./Footer/FooterOverlay";
+import Newsletter from "./Footer/Newsletter";
+import MenuItem from "./Menuitem/MenuItem";
+import Navbar from "./Navbar/Navbar";
+
+export { SubHeading, FooterOverlay, Newsletter, MenuItem, Navbar };
